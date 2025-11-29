ad2e7f0f25d94af9ef0bc9389f4b6cb9e623e9bb
Ivan Vrtarić <ivrtaric@gmail.com>
2025-11-29 20:16:56 +0100

    Basic repository setup

---
+++ vitest.config.ts
@@ -0,0 +1,8 @@
+/// <reference types="vitest/config" />
+import { configDefaults, defineConfig } from 'vitest/config';
+
+export default defineConfig({
+	test: {
+		exclude: [...configDefaults.exclude]
+	}
+});
